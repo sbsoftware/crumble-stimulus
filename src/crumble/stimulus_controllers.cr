bcc15b2bfbdb23911e25b513853cfb707b71ba5e
Stefan Bilharz <stefan@sbsoftware.de>
2024-11-29 16:16:45 +0100

    CRST-5 Register controllers on inheritance from base class

--- src/crumble/stimulus_controllers.cr
+++ src/crumble/stimulus_controllers.cr
@@ -1,13 +1,14 @@
 require "js"
 require "stimulus"
+require "crumble"
 
 module Crumble
   class StimulusControllers < JS::Module
     js_import Application, Controller, from: Crumble::Stimulus.stimulus_url
 
-    {% for ctrl_klass in ::Stimulus::Controller.all_subclasses %}
-      js_class {{ctrl_klass}}
-    {% end %}
+    def self.add_controller(klass)
+      js_class klass
+    end
 
     def_to_js do
       window.Stimulus = Application.start._call
@@ -17,11 +18,19 @@ module Crumble
       {% end %}
     end
 
-    @@asset_file = JavascriptFile.new("/assets/stimulus_controllers.js", self.to_js)
+    macro finished
+      @@asset_file = JavascriptFile.new("/assets/stimulus_controllers.js", self.to_js)
+    end
 
     def self.to_html_attrs(_tag, attrs)
       attrs["type"] = "module"
       attrs["src"] = @@asset_file.uri_path
     end
+  end
+end
+
+class Stimulus::Controller
+  macro inherited
+    Crumble::StimulusControllers.add_controller({{@type}})
   end
 end
